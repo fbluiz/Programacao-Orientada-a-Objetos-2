da2fa6cbafd51711ce5d5facb7e672113422da9b
brson <brson@FBLUIZ>
2022-09-11 23:37:23 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade5/Conta.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.Xml;
+
+namespace Atividade5
+{
+    internal class Conta
+    {
+
+        // Atributos 
+
+        public int NumeroConta { get; private set; }
+        public string TitularConta { get; set; }
+        public double SaldoConta { get; private set; }
+
+
+        // Construtores
+        public Conta(int numero_Conta, string titular_Conta, double deposito_inicial)
+        {
+            NumeroConta = numero_Conta;
+            TitularConta = titular_Conta;
+            Deposito(deposito_inicial);
+        }
+
+        public Conta (int numero_conta, string titular_conta) 
+        {
+            NumeroConta = numero_conta;
+            TitularConta = titular_conta;
+            SaldoConta = 0; 
+        
+        }
+
+        // Metodos e Funções
+        public void Deposito(double valor_deposito)
+        {
+            SaldoConta = SaldoConta + valor_deposito;
+        }
+
+        public void Saque(double valor_saque)
+        {
+            int taxa = 5;
+
+            SaldoConta = SaldoConta - valor_saque - taxa; 
+        }
+
+    public override string ToString()
+    {
+        return "Conta: " + NumeroConta + ", " + "titular: " + TitularConta + ", " + "Saldo: $ " + SaldoConta;
+    }
+
+       
+
+
+
+    }
+
+
+}
+
---
+++ Atividade5/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.IO;
+using System;
+
+namespace Atividade5
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+
+
+            Conta m;
+          
+
+            Console.WriteLine("Entre com o número da conta : \n");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o titular da conta : \n");
+            string titular = Console.ReadLine();
+            
+
+
+            Console.WriteLine("Haverá depósito inicial ? (s/n) \n ");
+            char opc = char.Parse(Console.ReadLine());
+            if (opc == 's')
+           
+            {
+                Console.WriteLine("Entre com o valor do deposito inicial");
+                double deposito = double.Parse(Console.ReadLine());
+
+                m = new Conta(numero, titular, deposito);
+                
+                Console.WriteLine(m);
+
+                Console.WriteLine("Entre um valor para depósito: \n");
+                double deposito2 = double.Parse(Console.ReadLine());
+                m.Deposito(deposito2);
+                Console.Write("Dados atualizados : \n");
+                Console.WriteLine(m);
+
+                Console.WriteLine("Entre um valor para Saque: \n");
+                double saque = double.Parse(Console.ReadLine());
+                m.Saque(saque);
+                Console.WriteLine("Dados atualizados: \n");
+                Console.WriteLine(m);
+
+            }
+
+            else
+            {
+                m = new Conta(numero, titular);
+                Console.WriteLine(m);
+                
+                Console.WriteLine("Entre um valor para depósito: \n");
+                double deposito = double.Parse(Console.ReadLine());
+                m.Deposito(deposito);
+                Console.WriteLine("Dados atualizados : \n");
+                Console.WriteLine(m);
+
+                Console.WriteLine("Entre um valor para Saque: \n");
+                double saque = double.Parse(Console.ReadLine());
+                m.Saque(saque);
+                Console.WriteLine("Dados atualizados: \n");
+                Console.WriteLine(m);
+
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+
+}
